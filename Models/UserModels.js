21fc87cb2fc89d16d177262e25578de39de3b101
Majedul Hasan <hasanmajedul@gmail.com>
2022-11-12 22:20:47 +0600

    UserMopdel

---
+++ Models/UserModels.js
@@ -0,0 +1,119 @@
+const crypto = require('crypto'); // crypto is a built-in Node module
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type : String,
+        trim: true, //white space trimed out
+        required: true,
+        max: 32,
+        unique:true,
+        index: true, //make it indexing
+        lowercase: true
+
+    },
+    name:{
+        type : String,
+        trim: true, //white space tremed out
+        required: true,
+        max: 32,
+    },
+    email:{
+        type : String,
+        trim: true, //white space tremed out
+        required: true,
+       
+        unique:true,
+        
+        lowercase: true
+
+    },
+    profile:{
+        type : String,       
+        required: true       
+
+    },
+    hashed_password :{
+        type : String,       
+        required: true 
+    },
+    salt:{
+        type: String
+    },
+    about:{
+        type: String
+        
+    },
+    role:{
+        type: Number,
+        default: 0
+
+    },
+    photo:{
+        data: Buffer, // binary data type
+        contentType: String
+    },
+    resetPasswordLink:{
+        data: String,
+        default:''
+    }
+
+},
+{
+    timestamps: true
+})
+
+
+
+userSchema.virtual('password')
+    .set(function(password){
+        // a temporary variable "_password" 
+        this._password = password
+
+        // generate salt
+        this.salt = this.makeSalt()
+
+        //encript password
+
+        this.hashed_password = this.encryptPassword(password)   // set this hashed_password by #encryptPassword method
+
+
+    } )
+    .get(function(){                                        //get method
+        return this._password                                       
+
+    })
+
+/** 
+ *  @makeSalt  &&  @encryptPassword  functions
+   */
+userSchema.methods = {
+    authenticate: function(plainText){
+        return this.encryptPassword(plainText) === this.hashed_password
+
+    },
+        encryptPassword: function(password){
+        if(!password) return ""
+
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+            
+        } catch (err) {
+            return ""            
+        }
+    },
+
+    makeSalt: function(){
+        return Math.round(new Date().valueOf()*Math.random()) + "";
+    }
+}
+
+
+const User = mongoose.model("User", userSchema)
+
+// export default User
+module.exports = User
